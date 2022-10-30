1c4d977a0de2ba60e76cf2a1812269f84b195d54
philnbr <93681431+philnbr@users.noreply.github.com>
2022-10-30 18:04:26 +0100

    Add files via upload

---
+++ SchnitzelWithFries.py
@@ -0,0 +1,5 @@
+class SchnitzelWithFries:
+    def getPrice(self):
+        return 8.99
+    def printDescription(self):
+        return "Nice schnitzel with nice fries"
---
+++ SchnitzelWithWedges.py
@@ -0,0 +1,5 @@
+class SchnitzelWithWedges:
+    def getPrice(self):
+        return 9.99
+    def printDescription(self):
+        return "Nice schnitzel with nice wedges"
