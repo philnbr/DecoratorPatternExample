0ce938b74b87592cccd5ca9d977dfd4ced643709
philnbr <93681431+philnbr@users.noreply.github.com>
2022-11-01 09:30:31 +0100

    Delete PatternTestSolution directory

--- PatternTestSolution/Interfaces/IDish.py
+++
@@ -1,7 +0,0 @@
-from abc import ABC, abstractmethod
-
-
-class IDish:
-    @abstractmethod
-    def getPrice(): raise NotImplementedError
-    def printDescription(): raise NotImplementedError
--- PatternTestSolution/Interfaces/ISideDish.py
+++
@@ -1,4 +0,0 @@
-import Interfaces.IDish as IDish
-class ISideDish(IDish):
-    def __init__(self, Dish : IDish):
-        self.__dish = Dish
